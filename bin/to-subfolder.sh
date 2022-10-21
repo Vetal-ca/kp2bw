b340a07b6074ecd2bb30c3f56e15422ca0e7f1a5
Vitali Khlebko <khlebko@yahoo.com>
2022-10-20 22:23:20 -0400

    To subfolder, WIP

--- bin/to-subfolder.sh
+++ bin/to-subfolder.sh
@@ -6,14 +6,15 @@ set -o pipefail
 
 usage ()
 {
-#    echo "Usage: ${0##*/} --folder|f folder --user|u email --password|p password [--org|o org-name] [--exclude|e folder-prefixes-to-exclude-csv]"
-    echo "Usage: ${0##*/} --folder|f folder --client-id|i client-id --client-secret|s client-secret  --url|u url [--org|o org-name] [--exclude|e folder-prefixes-to-exclude-csv]"
+    echo "Usage: ${0##*/} --folder|f folder --email|e email --password|p password [--org|o org-name]"
+#    echo "Usage: ${0##*/} --folder|f folder --client-id|i client-id --client-secret|s client-secret  --url|u url [--org|o org-name] [--exclude|e folder-prefixes-to-exclude-csv]"
     echo "Example: ${0##*/} --folder private --client-id some-id... --client-secret some-secret...  --url https://bw.somedomain.tld [--org|o org-name] [--exclude|e folder-prefixes-to-exclude-csv]"
 
     exit 1
 }
 
-OPTS=$(getopt --options f:i:s:e:u: --longoptions folder:,client-id:,client-secret:,exclude:,url: --name 'parse-options' -- "$@")
+OPTS=$(getopt --options f:e:p:o: --longoptions folder:,email:,password:,org: --name 'parse-options' -- "$@")
+#OPTS=$(getopt --options f:i:s:e:u: --longoptions folder:,client-id:,client-secret:,exclude:,url: --name 'parse-options' -- "$@")
 if [ $? != 0 ]; then
   echo "Failed parsing options." >&2
   exit 1
@@ -27,26 +28,31 @@ while true; do
     folder="$2"
     shift 2
     ;;
-  -i | --client-id)
-    client_id="$2"
+  -e | --email)
+    email="$2"
     shift 2
     ;;
-  -s | --client-secret)
-    client_secret="$2"
+  -p | --password)
+    password="$2"
     shift 2
     ;;
-  -u | --url)
-    url="$2"
-    shift 2
-    ;;
-#  -o | --org)
-#    org="$2"
+#  -i | --client-id)
+#    client_id="$2"
 #    shift 2
 #    ;;
-#  -e | --exclude)
-#    exclude="$2"
+#  -s | --client-secret)
+#    client_secret="$2"
 #    shift 2
 #    ;;
+#  -u | --url)
+#    url="$2"
+#    shift 2
+#    ;;
+  -o | --org)
+    org="$2"
+    shift 2
+    ;;
+
   --)
     shift
     break
@@ -60,20 +66,30 @@ if [ -z ${folder+set} ]; then
   usage
 fi
 
-if [ -z ${client_id+set} ]; then
-  echo "client-id is not set"
+if [ -z ${email+set} ]; then
+  echo "Folder is not set"
   usage
 fi
 
-if [ -z ${client_secret+set} ]; then
-  echo "client_secret is not set"
+if [ -z ${password+set} ]; then
+  echo "Folder is not set"
   usage
 fi
 
-if [ -z ${url+set} ]; then
-  echo "url is not set"
-  usage
-fi
+#if [ -z ${client_id+set} ]; then
+#  echo "client-id is not set"
+#  usage
+#fi
+#
+#if [ -z ${client_secret+set} ]; then
+#  echo "client_secret is not set"
+#  usage
+#fi
+#
+#if [ -z ${url+set} ]; then
+#  echo "url is not set"
+#  usage
+#fi
 
 # urldecode https://stackoverflow.com/questions/28309728/decode-url-in-bash
 # Encode
@@ -94,41 +110,53 @@ fi
 #  --header 'device-type: 8' \
 #  --data "${data}"
 
-#bw logout || true
-#echo "User ${user}, logging in"
-#session=$(bw login "${user}" "${password}" --raw)
+bw logout || true
+echo "User ${email}, logging in"
+session=$(bw login "${email}" "${password}" --raw)
 
-#if [ ! -z "${org-}" ]; then
-#  echo "Org is passed in, retrieving info"
-#  org_id=$(bw list organizations --session "${session}" | jq -r --arg org "${org}" '.[] | select(.name == $org) | .id')
-#  if [ -z "${org_id}" ]; then
-#    echo "Organization \"${org}\" is incorrect, bailing out ..."
-#  fi
-#fi
+if [ ! -z "${org-}" ]; then
+  echo "Org is passed in, retrieving info"
+  org_id=$(bw list organizations --session "${session}" | jq -r --arg org "${org}" '.[] | select(.name == $org) | .id')
+  if [ -z "${org_id}" ]; then
+    echo "Organization \"${org}\" is incorrect, bailing out ..."
+  fi
+fi
 
-#bw list folders --session "${session}" | jq -rc '.[] | select(.id != null)' | while read -r f; do
-#  name=$(echo "${f}" | jq -r '.name')
-#  id=$(echo "${f}" | jq -r '.id')
-#
-#  echo "Processing folder \"${name}\" ..."
-#  # https://stackoverflow.com/a/2172365/1672461
-#  if [[ ! "${name}" =~ ^"${folder}".*$ ]]; then
-#    new_name="${folder}/${name}"
-#    echo "Renaming folder: \"${name}\" => \"${new_name}\""
-#    data=$(jq -n --arg name "${new_name}" '{"name":$name}' | base64 --wrap=0)
-#    echo bw edit folder "${id}" "${data}" --session "${session}"
-#    bw get folder "${id}" --session "${session}"
-#    break
-#  fi
-#done
-#
+bw sync  --session "${session}"
+bw list folders --session "${session}" | jq -rc '.[] | select(.id != null)' | while read -r f; do
+  name=$(echo "${f}" | jq -r '.name')
+  id=$(echo "${f}" | jq -r '.id')
 
+  echo "Processing folder \"${name}\" ..."
+  # https://stackoverflow.com/a/2172365/1672461
+  if [[ ! "${name}" =~ ^"${folder}".*$ ]]; then
+    new_name="${folder}/${name}"
+    echo "Renaming folder: \"${name}\" => \"${new_name}\""
+    data=$(jq -n --arg name "${new_name}" '{"name":$name}' | base64 --wrap=0)
+    echo bw edit folder "${id}" "${data}" --session "${session}"
+    bw get folder "${id}" --session "${session}"
+    break
+  fi
+done
+
+
+# -------------- API
 #user="admin" &&\
 #info="/projects/personal/work/bw.enc.json" &&\
 #oauth_data=$(sops --decrypt "${info}" | jq -r ".bw.${user}.oauth") &&\
 #client_id=$(echo "${oauth_data}" | jq -r '.client_id') &&\
 #client_secret=$(echo "${oauth_data}" | jq -r '.client_secret')
 # dos2unix to-subfolder.sh && ./to-subfolder.sh --folder "Vitali & Kate shared" --client-id "${client_id}" --client-secret="${client_secret}" --url https://keestore.vetals.com
+
+
+# User/pass
+#user="admin" &&\
+#info="/projects/personal/work/bw.enc.json" &&\
+#user_data=$(sops --decrypt "${info}" | jq -r ".bw.${user}") &&\
+#email=$(echo "${user_data}" | jq -r '.email') &&\
+#password=$(echo "${user_data}" | jq -r '.password') &&\
+#dos2unix to-subfolder.sh && ./to-subfolder.sh --folder "Vitali & Kate shared" --email "${email}" --password="${password}"
+
 
 # https://bitwarden.com/help/personal-api-key/
 # Public API: https://bitwarden.com/help/api/
@@ -137,23 +165,23 @@ fi
 
 
 # Doesn't work without these (Error 500)
-device_identifier="c7ceab4f-09df-4d1e-81af-543460573ba3"
-device_name="linux"
-data="scope=api&client_id=${client_id}&grant_type=client_credentials&client_secret=${client_secret}&deviceName=${device_name}&deviceIdentifier=${device_identifier}"
-
-
-login_data=$(curl --request POST "${url}/identity/connect/token" \
-  --header 'Content-Type: application/x-www-form-urlencoded' \
-  --data "${data}" --silent)
-
-token=$(echo ${login_data} | jq -r '.access_token')
-
-curl --request GET "${url}/api/folders" \
-  --header 'Content-Type: application/json' \
-  --header "Authorization: Bearer ${token}" \
-  --silent | jq -r '.Data[]'
-
-curl --request GET "${url}//api/sync?excludeDomains=true" \
-  --header 'Content-Type: application/json' \
-  --header "Authorization: Bearer ${token}" \
-  --silent | jq
+#device_identifier="c7ceab4f-09df-4d1e-81af-543460573ba3"
+#device_name="linux"
+#data="scope=api&client_id=${client_id}&grant_type=client_credentials&client_secret=${client_secret}&deviceName=${device_name}&deviceIdentifier=${device_identifier}"
+#
+#
+#login_data=$(curl --request POST "${url}/identity/connect/token" \
+#  --header 'Content-Type: application/x-www-form-urlencoded' \
+#  --data "${data}" --silent)
+#
+#token=$(echo ${login_data} | jq -r '.access_token')
+#
+#curl --request GET "${url}/api/folders" \
+#  --header 'Content-Type: application/json' \
+#  --header "Authorization: Bearer ${token}" \
+#  --silent | jq -r '.Data[]'
+#
+#curl --request GET "${url}//api/sync?excludeDomains=true" \
+#  --header 'Content-Type: application/json' \
+#  --header "Authorization: Bearer ${token}" \
+#  --silent | jq
